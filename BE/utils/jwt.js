e7907db1742451af1cc6a8050291b7059be55d91
Cuong <cuong@Cuongs-MacBook-Pro-2.local>
2025-04-27 01:20:50 +0700

    update token

--- BE/utils/jwt.js
+++ BE/utils/jwt.js
@@ -5,24 +5,24 @@ dotenv.config();
 const { JWT_SECRET } = process.env;
 
 module.exports = {
-    createAccessToken: async (data) => {
-        return jwt.sign(data, JWT_SECRET, { expiresIn: "1h" });
-    },
+  createAccessToken: async (data) => {
+    return jwt.sign(data, JWT_SECRET, { expiresIn: "60d" });
+  },
 
-    verifyToken: async (token) => {
-        try {
-            return jwt.verify(token, JWT_SECRET);
-        } catch (error) {
-            console.error("Lỗi xác thực token:", error.message);
-            return null;
-        }
-    },
+  verifyToken: async (token) => {
+    try {
+      return jwt.verify(token, JWT_SECRET);
+    } catch (error) {
+      console.error("Lỗi xác thực token:", error.message);
+      return null;
+    }
+  },
 
-    createRefreshToken: async () => {
-        return jwt.sign(
-            { data: Math.random() + new Date().getTime() },
-            JWT_SECRET,
-            { expiresIn: "1d" }
-        );
-    },
+  createRefreshToken: async () => {
+    return jwt.sign(
+      { data: Math.random() + new Date().getTime() },
+      JWT_SECRET,
+      { expiresIn: "60d" }
+    );
+  },
 };
